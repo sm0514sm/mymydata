3de4327ee4b2411927377e1af927ef7a957717ac
이상민(**1659) / Data담당 검색추천팀 <211659@ssg.com>
2024-08-09 10:27:58 +0900

    Initial commit

---
+++ src/main/java/com/sangminlee/mymydata/MymydataApplication.java
@@ -0,0 +1,13 @@
+package com.sangminlee.mymydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymydataApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MymydataApplication.class, args);
+  }
+
+}
